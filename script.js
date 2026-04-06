58a8bf76da43a0766ffc703ab3e6ed8cd22308fa
NaNaMark <memeburrii2@gmail.com>
2026-04-06 18:07:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
 
+function playSong() {
+  audio.play();
+}
+
+function pauseSong() {
+  audio.pause();
+}
+
+function prevSong() {
+  audio.currentTime = 0;
+}
+
+function nextSong() {
+  audio.currentTime = audio.duration;
+}
+
+/* progress bar */
+audio.addEventListener("timeupdate", () => {
+  progress.value = (audio.currentTime / audio.duration) * 100;
+});
+
+progress.addEventListener("input", () => {
+  audio.currentTime = (progress.value / 100) * audio.duration;
+});
