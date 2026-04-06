1c264491c79c8e233d296c82ea0639bf72104e3b
NaNaMark <memeburrii2@gmail.com>
2026-04-06 18:15:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const audio = document.getElementById("audio");
 const progress = document.getElementById("progress");
 
-function playSong() {
-  audio.play();
-}
+/* player */
+function playSong(){ audio.play(); }
+function pauseSong(){ audio.pause(); }
+function prevSong(){ audio.currentTime = 0; }
+function nextSong(){ audio.currentTime = audio.duration; }
 
-function pauseSong() {
-  audio.pause();
-}
-
-function prevSong() {
-  audio.currentTime = 0;
-}
-
-function nextSong() {
-  audio.currentTime = audio.duration;
-}
-
-audio.addEventListener("timeupdate", () => {
+/* progress */
+audio.addEventListener("timeupdate", ()=>{
   progress.value = (audio.currentTime / audio.duration) * 100;
 });
 
-progress.addEventListener("input", () => {
+progress.addEventListener("input", ()=>{
   audio.currentTime = (progress.value / 100) * audio.duration;
+});
+
+/* scroll animation */
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", ()=>{
+  reveals.forEach(el=>{
+    let top = el.getBoundingClientRect().top;
+    if(top < window.innerHeight - 100){
+      el.classList.add("active");
+    }
+  });
 });
