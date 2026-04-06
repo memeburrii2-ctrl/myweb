82069b6aa8fea1c410466206a6d12bbfa2ec8c30
NaNaMark <memeburrii2@gmail.com>
2026-04-06 18:34:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-function toggleMenu(){
-  document.getElementById("menu").classList.toggle("active");
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for(let i=0;i<80;i++){
+  particles.push({
+    x:Math.random()*canvas.width,
+    y:Math.random()*canvas.height,
+    size:Math.random()*2,
+    speedY:Math.random()*0.5 + 0.2
+  });
 }
+
+function draw(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+
+  ctx.fillStyle="white";
+
+  particles.forEach(p=>{
+    ctx.beginPath();
+    ctx.arc(p.x,p.y,p.size,0,Math.PI*2);
+    ctx.fill();
+
+    p.y += p.speedY;
+
+    if(p.y > canvas.height){
+      p.y = 0;
+      p.x = Math.random()*canvas.width;
+    }
+  });
+
+  requestAnimationFrame(draw);
+}
+
+draw();
